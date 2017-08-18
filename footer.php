4e60005e51c6985c120aa9c7443c58990b9eccd2
Mayuko Moriyama <mayuko.jpn@gmail.com>
2017-08-18 15:06:09 +0900

    Use entire sentences
    In most languages, word order is different than English.
    https://developer.wordpress.org/themes/functionality/internationalization/#best-practices-for-writing-strings

--- footer.php
+++ footer.php
@@ -7,10 +7,11 @@
     <div class="copy-right"><p>&copy; <?php esc_html(date('Y'));?> <?php bloginfo('name'); ?></p></div>
     <div class="powerd-by">
       <p>
-        <?php esc_html_e('Proudly powered by ', 'gildrest');?>
-        <a href="<?php echo esc_url('https://wordpress.org/');?>" target="_blank"><?php esc_html_e('WordPress', 'gildrest');?></a>
-        <?php esc_html_e('. Theme by ', 'gildrest');?>
-        <a href="<?php echo esc_url('https://profiles.wordpress.org/shoyamane');?>" target="_blank"><?php esc_html_e('Gild Themes', 'gildrest');?></a>
+        <?php
+        printf( esc_html__( 'Proudly powered by %1$s. Theme by %2$s', 'gildrest' ),
+          '<a href="'. esc_html__( 'https://wordpress.org/', 'gildrest' ) .'" target="_blank">'. esc_html__( 'WordPress', 'gildrest' ) .'</a>',
+          '<a href="'. esc_html__('https://profiles.wordpress.org/shoyamane', 'gildrest' ) .'" target="_blank">'. esc_html__( 'Gild Themes', 'gildrest' ) .'</a>'
+        ); ?>
       </p>
     </div>
   </div>
